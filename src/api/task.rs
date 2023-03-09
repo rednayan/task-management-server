6992b88229c24ffcf2d758f100f76be572cbf1a2
nayan <nayansharma1010@gmail.com>
2023-03-09 17:55:57 +0530

    feat: dynamo db

---
+++ src/api/task.rs
@@ -0,0 +1,23 @@
+use actix_web::{
+    get,
+    post,
+    put,
+    error::ResponseError,
+    web::Path,
+    web::Json,
+    web::Data,
+    HttpResponse,
+    http::{header::ContentType,StatusCode}
+};
+use serde::{Serialize,Deserialize};
+use derive_more::{Display};
+
+pub struct TaskIdentifier {
+    task_global_id: String, 
+
+}
+
+#[get("/task/{task_global_id}")]
+pub async fn get_task(task_identifier:Path<TaskIdentifier>) -> Json<String> {
+    return  Json(task_identifier.into_inner().task_global_id);
+}
